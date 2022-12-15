e183d661e01f4cc55f4809fb67de3be3ffd34ce9
deimonbody <pavlov.d@donnu.edu.ua>
2022-12-15 11:20:55 +0200

    commit after review

--- client/src/hooks/isRightRoomID.ts
+++ client/src/hooks/isRightRoomID.ts
@@ -2,6 +2,5 @@ import { useAppSelector } from "@route/store/hooks";
 
 export const useRightRoomID = (currentRoomID: string) => {
   const { roomId } = useAppSelector((store) => store.gameReducer);
-  if (roomId && roomId === currentRoomID) return true;
-  return false;
+  return !!roomId && roomId === currentRoomID;
 };
