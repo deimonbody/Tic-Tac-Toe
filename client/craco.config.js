827e8364b47cdebb3bc2aec09e7d25138ce5d911
deimonbody <pavlov.d@donnu.edu.ua>
2022-12-15 16:20:39 +0200

    deleted console.log

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,7 +4,7 @@ const {CracoAliasPlugin, configPaths} = require('react-app-rewire-alias')
 const aliasMap = {
   '@src': './src',
 }
-console.log(aliasMap);
+
 module.exports = {
   plugins: [
     {
